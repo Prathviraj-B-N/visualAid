6b91181c1a7ffa1cb692c299e7e18756746f8d4d
prathviraj b <prathvirajbn.cs18@sahyadri.edu.in>
2020-05-16 22:11:23 +0530

    Update encode_faces.py

--- encode_faces.py
+++ encode_faces.py
@@ -7,31 +7,32 @@
 # import the necessary packages
 from imutils import paths
 import face_recognition
-import argparse
+#import argparse
 import pickle
 import cv2
 import os
 
 # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--dataset", required=True,
-	help="path to input directory of faces + images")
-ap.add_argument("-e", "--encodings", required=True,
-	help="path to serialized db of facial encodings")
-ap.add_argument("-d", "--detection-method", type=str, default="cnn",
-	help="face detection model to use: either `hog` or `cnn`")
-args = vars(ap.parse_args())
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--dataset", required=True,
+# 	help="path to input directory of faces + images")
+# ap.add_argument("-e", "--encodings", required=True,
+# 	help="path to serialized db of facial encodings")
+# ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+# 	help="face detection model to use: either `hog` or `cnn`")
+# args = vars(ap.parse_args())
 
 # grab the paths to the input images in our dataset
 print("[INFO] quantifying faces...")
-imagePaths = list(paths.list_images(args["dataset"]))
+#imagePaths = list(paths.list_images(args["dataset"]))
+imagePaths = list(paths.list_images("dataset"))
 
 # initialize the list of known encodings and known names
 knownEncodings = []
 knownNames = []
 
 # loop over the image paths
-for (i, imagePath) in enumerate(imagePaths):
+for (i, imagePath) in enumerate(imagePaths):            # (0, 'path1'), (1,'path2') ...
 	# extract the person name from the image path
 	print("[INFO] processing image {}/{}".format(i + 1,
 		len(imagePaths)))
@@ -45,7 +46,7 @@ for (i, imagePath) in enumerate(imagePaths):
 	# detect the (x, y)-coordinates of the bounding boxes
 	# corresponding to each face in the input image
 	boxes = face_recognition.face_locations(rgb,
-		model=args["detection_method"])
+		model='hog')
 
 	# compute the facial embedding for the face
 	encodings = face_recognition.face_encodings(rgb, boxes)
@@ -60,6 +61,6 @@ for (i, imagePath) in enumerate(imagePaths):
 # dump the facial encodings + names to disk
 print("[INFO] serializing encodings...")
 data = {"encodings": knownEncodings, "names": knownNames}
-f = open(args["encodings"], "wb")
+f = open("encodings.pickle", "wb")
 f.write(pickle.dumps(data))
 f.close()
